a31fbbaf06669782759cf87a85735bb06f2d0a48
A'Yanna Rouse <yanni620@bu.edu>
2024-08-19 18:32:28 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRAQl3AJjFrcjMrPqJVApjoC3FQB1v0OA",
-  authDomain: "brain-flip-c0c92.firebaseapp.com",
-  projectId: "brain-flip-c0c92",
-  storageBucket: "brain-flip-c0c92.appspot.com",
-  messagingSenderId: "369740210833",
-  appId: "1:369740210833:web:6ff9fe86da5c386cdcd530",
-  measurementId: "G-3F64VTG3EJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 let analytics;
 // Initialize Firebase
