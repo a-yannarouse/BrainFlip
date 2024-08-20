a54b9bd2469a877fc1d56d2ebfda7c94cc04f52a
A'Yanna Rouse <yanni620@bu.edu>
2024-08-19 20:19:36 -0400

    updated links

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -60,6 +60,7 @@ export async function POST(req){
         
         const checkoutSession = await stripe.checkout.sessions.create(params);
         return NextResponse.json(checkoutSession, { status: 200 });
+        
     } catch (error) {
         console.error('Error creating checkout session:', error);
         return NextResponse.json({ error: { message: error.message } }, { status: 500 });
